c2e9bb574b2711c3ba0dc3ec18f817182cbefe45
Bobby Naluz <bnaluz9@gmail.com>
2023-10-07 17:48:00 -0400

    initial commit

---
+++ practice-for-week-04-adventure-project-long-practice/class/item.js
@@ -0,0 +1,13 @@
+class Item {
+  // Item Class: Item should have name and description attributes
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+  }
+
+  // Your code here
+}
+
+module.exports = {
+  Item,
+};
